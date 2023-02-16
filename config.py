30562fc6cfd56f7119aed4d3a6e34300cba9a47a
Dark Litss <ls1102086624@outlook.com>
2023-02-16 17:42:20 +0800

    Revert "support access_token for revChatGPT.V1"
    This reverts commit d51ad5b8067055e9a500f4542b40425bd51f58f0.

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ class Mirai(BaseModel):
     ws_url: str = "http://localhost:8080"
     """mirai-api-http 的 ws 适配器地址"""
 class OpenAI(BaseModel):
-    accounts: List[Union[OpenAIEmailAuth, OpenAISessionTokenAuth, OpenAIAccessTokenAuth]]
+    accounts: List[Union[OpenAIEmailAuth, OpenAISessionTokenAuth]]
 
 class OpenAIAuthBase(BaseModel):
     mode: str = "browser"
@@ -48,10 +48,6 @@ class OpenAIEmailAuth(OpenAIAuthBase):
 class OpenAISessionTokenAuth(OpenAIAuthBase):
     session_token: str
     """OpenAI 的 session_token"""
-
-class OpenAIAccessTokenAuth(OpenAIAuthBase):
-    access_token: str
-    """OpenAI 的 access_token"""
 
 class OpenAIAPIKey(OpenAIAuthBase):
     api_key: str
@@ -141,7 +137,7 @@ class Preset(BaseModel):
 
 class Config(BaseModel):
     mirai: Mirai
-    openai: Union[OpenAI, OpenAIEmailAuth, OpenAISessionTokenAuth, OpenAIAccessTokenAuth]
+    openai: Union[OpenAI, OpenAIEmailAuth, OpenAISessionTokenAuth]
     text_to_image: TextToImage = TextToImage()
     trigger: Trigger = Trigger()
     response: Response = Response()
